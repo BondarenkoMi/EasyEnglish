5fc7c6cdf48461fb34e4025b31657c3927fe5272
Bondarenko Mikhail <mihail.bondarenko.2006@mail.ru>
2024-07-04 14:31:06 +0300

    1

---
+++ app/src/main/java/ru/itis/easyenglish/MainActivity.kt
@@ -0,0 +1,12 @@
+package ru.itis.easyenglish
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
