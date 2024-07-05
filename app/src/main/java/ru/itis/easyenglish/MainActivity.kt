84fb1863584254fe27ec8c2eeee4ff14b8664223
Bondarenko Mikhail <mihail.bondarenko.2006@mail.ru>
2024-07-05 13:25:40 +0300

    приложение запускается и видно список уровней

--- app/src/main/java/ru/itis/easyenglish/MainActivity.kt
+++ app/src/main/java/ru/itis/easyenglish/MainActivity.kt
@@ -2,11 +2,18 @@ package ru.itis.easyenglish
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import ru.itis.easyenglish.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
